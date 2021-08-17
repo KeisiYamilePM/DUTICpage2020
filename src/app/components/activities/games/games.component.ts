a75fc40229fd4e2b882fcdb5475b605a340b2392
Kevin <kevin.08.24.11@gmail.com>
2021-08-17 14:26:58 -0500

    Games page design

--- src/app/components/activities/games/games.component.ts
+++ src/app/components/activities/games/games.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { PRIMARY_OUTLET } from '@angular/router';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
-  styleUrls: ['./games.component.css']
+  styleUrls: ['./games.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
 export class GamesComponent implements OnInit {
 
-  constructor() { }
+  images = ['https://i.ibb.co/18p43Nx/banner-olimpiadas-v1.jpg','https://i.ibb.co/7gy4Jpj/Olimpiadas.jpg'];
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 7000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'});
+}
 
   ngOnInit(): void {
   }
